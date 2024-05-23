6f75f959c83c9b3ac500abebc34705b93c407d9f
Szymii <szymimelzer9@gmail.com>
2024-05-23 15:40:46 +0200

    Turn of forecast screen

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ Live [https://szymii-weather-app.netlify.app](https://szymii-weather-app.netlify
 - [Styled Components](https://styled-components.com/)
 - [Custom Webpack Config](https://webpack.js.org/)
 - [OpenWeather API](https://openweathermap.org/)
+
+## Run on your own
+
+- You will probably need node v16
+- This project is a little bit legacy so to instal use `npm install --legacy-peer-deps`
+- create .env file and add `API_KEY="your_open_weather_api_key"`
+- run `npm start` and open `http://localhost:8080/`
--- src/views/View.js
+++ src/views/View.js
@@ -1,60 +1,61 @@
-import React from 'react';
-import styled from 'styled-components';
-import Main from '../components/organisms/Main/Main';
-import Details from '../components/organisms/Details/Details';
-import Forecast from '../components/organisms/Forecast/Forecast';
-import Navigation from '../components/molecules/Navigation/Navigation';
+import React from "react";
+import styled from "styled-components";
+import Main from "../components/organisms/Main/Main";
+import Details from "../components/organisms/Details/Details";
+// import Forecast from "../components/organisms/Forecast/Forecast";
+import Navigation from "../components/molecules/Navigation/Navigation";
 
 const Wrapper = styled.main`
-	margin-top: 20vh;
-	display: flex;
-	height: 80vh;
-	scroll-behavior: smooth;
-	scroll-snap-type: x mandatory;
-	overflow-x: scroll;
-	background-color: ${({ theme }) => theme.colors.white};
-	transition: background-color 0.4s ease, color 0.4s ease;
+  margin-top: 20vh;
+  display: flex;
+  height: 80vh;
+  scroll-behavior: smooth;
+  scroll-snap-type: x mandatory;
+  overflow-x: scroll;
+  background-color: ${({ theme }) => theme.colors.white};
+  transition: background-color 0.4s ease, color 0.4s ease;
 
-	@media (min-width: ${({ theme }) => theme.media.desktop}) {
-		width: 100vw;
-		flex-direction: column;
-		scroll-snap-type: y mandatory;
-		overflow-y: scroll;
-		overflow-x: auto;
-	}
+  @media (min-width: ${({ theme }) => theme.media.desktop}) {
+    width: 100vw;
+    flex-direction: column;
+    scroll-snap-type: y mandatory;
+    overflow-y: scroll;
+    overflow-x: auto;
+  }
 `;
 
 const StyledSection = styled.section`
-	flex: none;
-	width: 100vw;
-	scroll-snap-align: start;
-	padding: 0 1.6em;
+  flex: none;
+  width: 100vw;
+  scroll-snap-align: start;
+  padding: 0 1.6em;
 
-	@media (min-width: ${({ theme }) => theme.media.desktop}) {
-		width: 100%;
-		max-width: 60em;
-		height: 80vh;
-		min-height: 30em;
-		margin: 0 auto;
-		padding: 0;
-	}
+  @media (min-width: ${({ theme }) => theme.media.desktop}) {
+    width: 100%;
+    max-width: 60em;
+    height: 80vh;
+    min-height: 30em;
+    margin: 0 auto;
+    padding: 0;
+  }
 `;
 
 const View = () => {
-	return (
-		<Wrapper>
-			<Navigation />
-			<StyledSection>
-				<Main />
-			</StyledSection>
-			<StyledSection>
-				<Details />
-			</StyledSection>
-			<StyledSection>
+  return (
+    <Wrapper>
+      <Navigation />
+      <StyledSection>
+        <Main />
+      </StyledSection>
+      <StyledSection>
+        <Details />
+      </StyledSection>
+      {/* openweathermap.org no longer provides free forecast :sad: */}
+      {/* <StyledSection>
 				<Forecast />
-			</StyledSection>
-		</Wrapper>
-	);
+			</StyledSection> */}
+    </Wrapper>
+  );
 };
 
 export default View;
